86c84b7063effaf6bfffd43e37a52b94a22347d6
Bobby Bouwmann <bobbybouwmann@gmail.com>
2016-01-03 11:56:18 +0100

    Rename some stuff

--- src/main/java/nl/ap/hillclimbing/Queens.java
+++ src/main/java/nl/ap/hillclimbing/Queens.java
@@ -2,17 +2,17 @@ package nl.ap.hillclimbing;
 
 public class Queens {
 
-    public static boolean isConsistent(int[] q, int n) {
-        for (int i = 0; i < n; i++) {
-            if (q[i] == q[n]) {
+    public static boolean isConsistent(int[] queens, int number) {
+        for (int i = 0; i < number; i++) {
+            if (queens[i] == queens[number]) {
                 return false; // same column
             }
 
-            if ((q[i] - q[n]) == (n - i)) {
+            if ((queens[i] - queens[number]) == (number - i)) {
                 return false; // same major diagonal
             }
 
-            if ((q[n] - q[i]) == (n - i)) {
+            if ((queens[number] - queens[i]) == (number - i)) {
                 return false; // same minor diagonal
             }
         }
@@ -21,10 +21,10 @@ public class Queens {
     }
 
     public static void printQueens(int[] q) {
-        int N = q.length;
+        int length = q.length;
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
                 if (q[i] == j) {
                     System.out.print("Q  ");
                 } else {
@@ -38,25 +38,25 @@ public class Queens {
         System.out.println();
     }
 
-    public static void calculate(int N) {
-        int[] a = new int[N];
+    public static void calculate(int length) {
+        int[] a = new int[length];
 
         calculate(a, 0);
     }
 
-    public static boolean calculate(int[] q, int n) {
-        int N = q.length;
+    public static boolean calculate(int[] queens, int number) {
+        int length = queens.length;
 
-        if (n == N) {
-            printQueens(q);
+        if (number == length) {
+            printQueens(queens);
 
             return true;
         } else {
-            for (int i = 0; i < N; i++) {
-                q[n] = i;
+            for (int i = 0; i < length; i++) {
+                queens[number] = i;
 
-                if (isConsistent(q, n)) {
-                    if (calculate(q, n + 1)) {
+                if (isConsistent(queens, number)) {
+                    if (calculate(queens, number + 1)) {
                         return true;
                     }
                 }
